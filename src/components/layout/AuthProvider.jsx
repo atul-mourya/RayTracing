85e017af0176c092d1a6eb725b30dbf5236e90ec
atul.mourya@gmail.com <atul.mourya@gmail.com>
2024-12-01 21:43:53 +0530

    Update AuthProvider to set redirectTo to window.location.origin

--- src/components/layout/AuthProvider.jsx
+++ src/components/layout/AuthProvider.jsx
@@ -57,6 +57,7 @@ const AuthProvider = ( { children } ) => {
 						supabaseClient={supabase}
 						appearance={{ theme: ThemeSupa }}
 						providers={[ 'google' ]}
+						redirectTo='window.location.origin'
 					/>
 				</DialogContent>
 			</Dialog>
